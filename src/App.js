894b6545895c4adc6f954cdcba099409e1a7b120
sofiyullahj <sofiyullahj@gmail.com>
2024-06-28 18:44:39 +0100

    refactor conversion functionality

--- src/App.js
+++ src/App.js
@@ -10,94 +10,85 @@ class App extends Component {
     }
   };
 
-  handleChange = e => {
-    const input = { ...this.state.input };
-    input[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ input });
+  handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState((prevState) => ({
+      input: {
+        ...prevState.input,
+        [name]: value
+      }
+    }));
+  };
+
+  getConvertedValue = () => {
+    const { baseInput, baseName, quoteName } = this.state.input;
+    const rates = {
+      USD: { NGN: 1500, EUR: 0.89, USD: 1 },
+      NGN: { USD: 0.00067, EUR: 0.00061, NGN: 1 },
+      EUR: { NGN: 1645, USD: 1.12, EUR: 1 }
+    };
+    return baseInput * (rates[baseName.slice(0, 3)][quoteName.slice(0, 3)] || 0);
   };
 
   render() {
-    const { baseInput, baseName, quoteName } = this.state.input;
+    const { quoteName } = this.state.input;
     return (
-      <React.Fragment>
-        <div className="App">
-          <h1 class="text-center curr">React Currency Converter</h1>
-          <div class="formWrapper bg-dark rounded-top border border-danger">
-            <form autoComplete="off">
-              <div class="form-group row">
-                <label for="exampleFormControlSelect1" class="col-3 ml-2 text-white">
-                  From
-                </label>
-                <input
-                  type="text"
-                  name="baseInput"
-                  onChange={this.handleChange}
-                  class="col-4 mr-1"
-                />
-                <select
-                  name="baseName"
-                  class="btn btn-primary"
-                  id="baseName"
-                  onChange={this.handleChange}
-                >
-                  <option selected disabled>
-                    Currency
-                  </option>
-                  <option value="USD1">USD</option>
-                  <option value="NGN1">NGN</option>
-                  <option value="EUR1">EUR</option>
-                </select>
-              </div>
-              <div class="form-group row">
-                <label for="exampleFormControlSelect1" class="col-3 ml-2 text-white">
-                  To
-                </label>
-                <input
-                  type="text"
-                  name="quoteInput"
-                  class="col-4 mr-1"
-                  onChange={this.handleChange}
-                  disabled
-                  value={
-                    baseName === "USD1" && quoteName === "NGN2"
-                      ? baseInput * 1500
-                      : baseName === "USD1" && quoteName === "EUR2"
-                      ? baseInput * 0.89
-                      : baseName === "USD1" && quoteName === "USD2"
-                      ? baseInput
-                      : baseName === "NGN1" && quoteName === "USD2"
-                      ? baseInput * 0.00067
-                      : baseName === "NGN1" && quoteName === "EUR2"
-                      ? baseInput * 0.0025
-                      : baseName === "NGN1" && quoteName === "NGN2"
-                      ? baseInput
-                      : baseName === "EUR1" && quoteName === "NGN2"
-                      ? baseInput * 403.43
-                      : baseName === "EUR1" && quoteName === "USD2"
-                      ? baseInput * 1.12
-                      : baseName === "EUR1" && quoteName === "EUR2"
-                      ? baseInput
-                      : null
-                  }
-                />
-                <select
-                  name="quoteName"
-                  class="btn btn-primary"
-                  id="quoteName"
-                  onChange={this.handleChange}
-                >
-                  <option selected disabled>
-                    Currency
-                  </option>
-                  <option value="USD2">USD</option>
-                  <option value="NGN2">NGN</option>
-                  <option value="EUR2">EUR</option>
-                </select>
-              </div>
-            </form>
-          </div>
+      <div className="App">
+        <h1 className="text-center curr">React Currency Converter</h1>
+        <div className="formWrapper bg-dark rounded-top border border-danger">
+          <form autoComplete="off">
+            <div className="form-group row">
+              <label htmlFor="baseName" className="col-3 ml-2 text-white">
+                From
+              </label>
+              <input
+                type="text"
+                name="baseInput"
+                onChange={this.handleChange}
+                className="col-4 mr-1"
+              />
+              <select
+                name="baseName"
+                className="btn btn-primary"
+                id="baseName"
+                onChange={this.handleChange}
+              >
+                <option value="" disabled selected>
+                  Currency
+                </option>
+                <option value="USD1">USD</option>
+                <option value="NGN1">NGN</option>
+                <option value="EUR1">EUR</option>
+              </select>
+            </div>
+            <div className="form-group row">
+              <label htmlFor="quoteName" className="col-3 ml-2 text-white">
+                To
+              </label>
+              <input
+                type="text"
+                name="quoteInput"
+                className="col-4 mr-1"
+                value={quoteName ? this.getConvertedValue() : ""}
+                disabled
+              />
+              <select
+                name="quoteName"
+                className="btn btn-primary"
+                id="quoteName"
+                onChange={this.handleChange}
+              >
+                <option value="" disabled selected>
+                  Currency
+                </option>
+                <option value="USD2">USD</option>
+                <option value="NGN2">NGN</option>
+                <option value="EUR2">EUR</option>
+              </select>
+            </div>
+          </form>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
